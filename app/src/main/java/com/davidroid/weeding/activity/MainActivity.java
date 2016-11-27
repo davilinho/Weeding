fc21f8d2512e52a8f2d6d93802f6a45d6a4b9fe3
David <davilinho@gmail.com>
2016-11-27 20:22:01 +0100

    Confirmation layout

--- app/src/main/java/com/davidroid/weeding/activity/MainActivity.java
+++ app/src/main/java/com/davidroid/weeding/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package com.davidroid.weeding;
+package com.davidroid.weeding.activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+
+import com.davidroid.weeding.R;
 
 public class MainActivity extends AppCompatActivity {
 
